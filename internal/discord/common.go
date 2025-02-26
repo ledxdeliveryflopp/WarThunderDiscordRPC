666f653e42d59c200d1e532b66e19cfe6b71e9a4
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-02-26 22:26:22 +0700

    Добавлены списки с названиями техники
    Списки содержать более читаемые названия

---
+++ internal/discord/common.go
@@ -0,0 +1,14 @@
+package discord
+
+import "time"
+
+var VehicleAirDict = map[string]string{
+	"f_16c_block_50": "F-16C Block 50",
+}
+
+var VehicleGroundDict = map[string]string{
+	"jp_type_90b_camo":      "Type 90 (B) \"Fuji\"",
+	"germ_leopard_2a4m_can": "Leopard 2A4M CAN",
+}
+
+var GameTime = time.Now()

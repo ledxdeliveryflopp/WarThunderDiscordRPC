949e57ae69f3a8a83c50e3da836ce623c47a40e3
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-02-27 15:49:02 +0700

    Дополнил список техники

--- internal/discord/common.go
+++ internal/discord/common.go
@@ -8,10 +8,13 @@ var VehicleAirDict = map[string]string{
 }
 
 var VehicleGroundDict = map[string]string{
-	"jp_type_90b_camo":      "Type 90 (B) \"Fuji\"",
-	"germ_leopard_2a4m_can": "Leopard 2A4M CAN",
-	"germ_leopard_2a7v":     "Leopard 2A7V",
-	"germ_leopard_2a5_pso":  "Leopard 2 PSO",
+	"jp_type_90b_camo":           "Type 90 (B) \"Fuji\"",
+	"germ_leopard_2a4m_can":      "Leopard 2A4M CAN",
+	"germ_leopard_2a7v":          "Leopard 2A7V",
+	"germ_leopard_2a5_pso":       "Leopard 2 PSO",
+	"us_m1128_wolfpack":          "M1128 Wolfpack",
+	"us_m1a1_hc_usmc":            "M1A1 Click-Bait",
+	"germ_leopard_2a4_pzbtl_123": "Leopard 2 (PzBtl 123)",
 }
 
 var GameTime = time.Now()

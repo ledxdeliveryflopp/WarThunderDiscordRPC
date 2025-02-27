04acbe270c27d3acb78f6687ba0fd5be266a7148
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-02-27 15:53:16 +0700

    Добавил разделение для логирование
    Инициализация разделяется "-----" как и ошибки установки статуса

--- cmd/wt/main.go
+++ cmd/wt/main.go
@@ -4,7 +4,10 @@ import (
 	config "WT_rich_presence/internal/configs"
 	discordHanlder "WT_rich_presence/internal/discord/hanlder"
 	discordInit "WT_rich_presence/internal/discord/init"
+	collector "WT_rich_presence/internal/tools"
 	"fmt"
+	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 func main() {
@@ -13,8 +16,14 @@ func main() {
 	httpClient := config.InitHttpClient()
 	err := discordInit.ConnectDiscordRPC("1344195597485211790")
 	if err != nil {
+		fmt.Println("Error while connecting to Discord RPC.")
+		time.Sleep(5 * time.Second)
+		log.Panicln(err)
 		return
 	}
+	collector.InitCollectorTxt()
 	fmt.Println("Start updating presence")
+	log.Println("start logging main loop")
+	log.Println("------------------------------------")
 	discordHanlder.RunUpdatePresenceLoop(&settings, *httpClient)
 }

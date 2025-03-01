c1096f6eeeace373bb7b3677576be03489b29888
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-03-01 18:53:48 +0700

    Обновил инициализацию настроек
    1) Добавил альтернативное отображение статистики техники
    2) Добавил установку локализации
    3) Теперь приложение не закрывается если не установлены настройки, а устанавливает их автоматически в зависимости от их отсутствия

--- internal/configs/settings.go
+++ internal/configs/settings.go
@@ -3,6 +3,7 @@ package configs
 import (
 	"encoding/json"
 	"fmt"
+	langOs "github.com/cloudfoundry-attic/jibber_jabber"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
@@ -12,6 +13,8 @@ import (
 type PresenceSettings struct {
 	RefreshTime   time.Duration `json:"refresh_time"`
 	MainLogoTheme string        `json:"main_logo_theme"`
+	AltPresence   bool          `json:"alt_presence"`
+	Lang          string        `json:"lang"`
 }
 
 func InitPresenceSettings() PresenceSettings {
@@ -32,21 +35,37 @@ func InitPresenceSettings() PresenceSettings {
 		log.Panicln(err)
 	case settings == (PresenceSettings{}):
 		fmt.Println("Empty settings file")
-		fmt.Println("Close application...")
-		time.Sleep(5 * time.Second)
-		os.Exit(3)
+		fmt.Println("Set basic settings")
+		settings.Lang = "en"
+		settings.AltPresence = false
+		settings.RefreshTime = 7
+		settings.MainLogoTheme = "main_white"
+		log.Println("Set basic settings success")
 	case settings.RefreshTime < 5:
 		fmt.Println("The refresh timer cannot be less than 5")
-		fmt.Println("Close application...")
-		time.Sleep(5 * time.Second)
-		os.Exit(3)
+		fmt.Println("Set 7 sec refresh time")
+		settings.RefreshTime = 7
 	case settings.MainLogoTheme != "main_red" == true:
 		if settings.MainLogoTheme != "main_white" == true {
 			fmt.Println("Bad main logo key, set white theme")
 			settings.MainLogoTheme = "main_white"
 		}
+	case settings.Lang != "ru" == true || settings.Lang != "en" == true:
+		systemName, err := langOs.DetectLanguage()
+		if err != nil {
+			fmt.Println("Error while get system lang, set en lang")
+			log.Errorf("error while get system lang: %s", err)
+			settings.Lang = "en"
+		} else if systemName == "ru" == true || systemName == "en" == true {
+			fmt.Printf("Detected system lang, lang code - %s. Set %s lang. \n", systemName, systemName)
+			settings.Lang = systemName
+		} else {
+			fmt.Printf("unknown system lang - %s, set en lang \n", systemName)
+			settings.Lang = "en"
+		}
 	}
 	log.Println("settings inited")
+	log.Infof("Settings: %+v", settings)
 	return settings
 }
 

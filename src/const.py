005229947a4dcccefc6dadf1fbc3b6cb1bff7253
LedxDelivery <korstim18@gmail.com>
2025-12-18 21:57:05 +0700

    1) опечатка

--- src/const.py
+++ src/const.py
@@ -4,7 +4,7 @@ class Lang:
     details: dict = {'en': 'Plays on', 'ru': 'Играет на'}
     tas_speed: dict = {'en': 'TAS speed', 'ru': 'Скорость TAS'}
     ias_speed: dict = {'en': 'IAS speed', 'ru': 'Скорость IAS'}
-    radio_altitude: dict = {'en': 'Radio altitude', 'ru': 'Радио высота'}
+    radio_altitude: dict = {'en': 'Radio altitude', 'ru': 'Радиовысота'}
     absolute_altitude: dict = {'en': 'Absolute altitude', 'ru': 'Абсолютная высота'}
     tank_speed: dict = {'en': 'Speed', 'ru': 'Скорость'}
     tank_crew: dict = {'en': 'Crew', 'ru': 'Экипаж'}
